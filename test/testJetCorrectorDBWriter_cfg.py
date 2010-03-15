e0851dea609987b4921232f0136fcabc57d45bfa
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-03-15 08:32:58 +0000

    Test configuration for writing a JEC.db file

--- test/testJetCorrectorDBWriter_cfg.py
+++ test/testJetCorrectorDBWriter_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("myprocess")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 
-process.CondDBCommon.connect = 'sqlite_file:JEC.db'
+process.CondDBCommon.connect = 'sqlite_file:JEC_Summer09_7TeV_ReReco332.db'
 
 
 process.maxEvents = cms.untracked.PSet(
@@ -12,21 +12,29 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("EmptySource")
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
-                                              process.CondDBCommon,
-                                              toPut = cms.VPSet(
-            cms.PSet(
-            record = cms.string('someCorrectiongJ'),
-                    tag = cms.string('dunno'),
-                    label = cms.string('dunno')
-            )
+            process.CondDBCommon,
+            toPut = cms.VPSet(
+              cms.PSet(record = cms.string('L2Relative_AK5Calo'),
+                       tag    = cms.string('L2Relative_AK5Calo'),
+                       label  = cms.string('L2Relative_AK5Calo')
+              ),
+              cms.PSet(record = cms.string('L3Absolute_AK5Calo'),
+                       tag    = cms.string('L3Absolute_AK5Calo'),
+                       label  = cms.string('L3Absolute_AK5Calo')
+              )
             )
 )
 
-process.mywriter = cms.EDAnalyzer("JetCorrectorDBWriter",
-                                  inputTxtFile = cms.untracked.string('input.txt'),
-                                  option = cms.untracked.string('gJ'),
-                                  label = cms.untracked.string('someCorrection')
-                                  )
+process.dbWriterL2AK5Calo = cms.EDAnalyzer("JetCorrectorDBWriter",
+  inputTxtFile = cms.untracked.string('Summer09_7TeV_ReReco332_L2Relative_AK5Calo.txt'),
+  option       = cms.untracked.string(''),
+  label        = cms.untracked.string('L2Relative_AK5Calo')
+)
 
+process.dbWriterL3AK5Calo = cms.EDAnalyzer("JetCorrectorDBWriter",
+  inputTxtFile = cms.untracked.string('Summer09_7TeV_ReReco332_L3Absolute_AK5Calo.txt'),
+  option       = cms.untracked.string(''),
+  label        = cms.untracked.string('L3Absolute_AK5Calo')
+)
 
-process.p = cms.Path(process.mywriter)
+process.p = cms.Path(process.dbWriterL2AK5Calo * process.dbWriterL3AK5Calo)
