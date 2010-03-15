ae9b38a1646c73217ebe98f79f8925a9072eb1dd
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-03-15 08:33:25 +0000

    Test configuration for reading JEC corrections from CondDB

--- test/testJetCorrectorDBReader_cfg.py
+++ test/testJetCorrectorDBReader_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("myprocess")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 
-process.CondDBCommon.connect = 'sqlite_file:JEC.db'
+process.CondDBCommon.connect = 'sqlite_file:JEC_Summer09_7TeV_ReReco332.db'
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
@@ -14,14 +14,17 @@ process.source = cms.Source("EmptySource")
 process.PoolDBESSource = cms.ESSource("PoolDBESSource",
         process.CondDBCommon,
         toGet = cms.VPSet(
-        cms.PSet(
-        record = cms.string('JetCorrectionsRecord'),
-            tag = cms.string('dunno'),
-            label = cms.untracked.string('L2Relative_AK5Calo_')
-            ))
-
+          cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('L2Relative_AK5Calo'),
+            label  = cms.untracked.string('L2Relative_AK5Calo')
+          )
+        )
 )
 
-process.demo2 = cms.EDAnalyzer('JetCorrectorDBReader', label = cms.untracked.string('L2Relative_AK5Calo_'))
+process.demo2 = cms.EDAnalyzer('JetCorrectorDBReader', 
+        label          = cms.untracked.string('L2Relative_AK5Calo'),
+        createTextFile = cms.untracked.bool(True)
+)
 
 process.p = cms.Path(process.demo2)
