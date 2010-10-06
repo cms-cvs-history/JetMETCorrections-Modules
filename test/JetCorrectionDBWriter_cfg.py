19af89d13a5542b154e58f3bbc0c2302c40367e9
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-10-06 08:53:07 +0000

    Updated to the Spring10 JEC

--- test/JetCorrectionDBWriter_cfg.py
+++ test/JetCorrectionDBWriter_cfg.py
@@ -8,135 +8,163 @@ process.PoolDBOutputService = cms.Service('PoolDBOutputService',
    process.CondDBCommon, 
    toPut = cms.VPSet( 
       cms.PSet(
-         record = cms.string('AK5Calo'), 
-         tag    = cms.string('AK5Calo'), 
-         label  = cms.string('AK5Calo') 
+         record = cms.string('JEC_Spring10_AK5Calo'), 
+         tag    = cms.string('JEC_Spring10_AK5Calo'), 
+         label  = cms.string('JEC_Spring10_AK5Calo') 
       ),
       cms.PSet(
-         record = cms.string('AK5PF'), 
-         tag    = cms.string('AK5PF'), 
-         label  = cms.string('AK5PF') 
+         record = cms.string('JEC_Spring10DataV2_AK5Calo'), 
+         tag    = cms.string('JEC_Spring10DataV2_AK5Calo'), 
+         label  = cms.string('JEC_Spring10DataV2_AK5Calo') 
       ),
       cms.PSet(
-         record = cms.string('AK5JPT'), 
-         tag    = cms.string('AK5JPT'), 
-         label  = cms.string('AK5JPT') 
+         record = cms.string('JEC_Spring10_AK5PF'), 
+         tag    = cms.string('JEC_Spring10_AK5PF'), 
+         label  = cms.string('JEC_Spring10_AK5PF') 
       ),
       cms.PSet(
-         record = cms.string('AK5TRK'), 
-         tag    = cms.string('AK5TRK'), 
-         label  = cms.string('AK5TRK') 
+         record = cms.string('JEC_Spring10DataV2_AK5PF'), 
+         tag    = cms.string('JEC_Spring10DataV2_AK5PF'), 
+         label  = cms.string('JEC_Spring10DataV2_AK5PF') 
       ),
       cms.PSet(
-         record = cms.string('AK7Calo'), 
-         tag    = cms.string('AK7Calo'), 
-         label  = cms.string('AK7Calo') 
+         record = cms.string('JEC_Spring10_AK5JPT'), 
+         tag    = cms.string('JEC_Spring10_AK5JPT'), 
+         label  = cms.string('JEC_Spring10_AK5JPT') 
       ),
       cms.PSet(
-         record = cms.string('AK7PF'), 
-         tag    = cms.string('AK7PF'), 
-         label  = cms.string('AK7PF') 
+         record = cms.string('JEC_Summer10_AK5JPT'), 
+         tag    = cms.string('JEC_Summer10_AK5JPT'), 
+         label  = cms.string('JEC_Summer10_AK5JPT') 
       ),
       cms.PSet(
-         record = cms.string('IC5Calo'), 
-         tag    = cms.string('IC5Calo'), 
-         label  = cms.string('IC5Calo') 
+         record = cms.string('JEC_Spring10DataV2_AK5JPT'), 
+         tag    = cms.string('JEC_Spring10DataV2_AK5JPT'), 
+         label  = cms.string('JEC_Spring10DataV2_AK5JPT') 
+      ),
+      cms.PSet(
+         record = cms.string('JEC_Spring10_AK5TRK'), 
+         tag    = cms.string('JEC_Spring10_AK5TRK'), 
+         label  = cms.string('JEC_Spring10_AK5TRK') 
+      ),
+      cms.PSet(
+         record = cms.string('JEC_Spring10_AK7Calo'), 
+         tag    = cms.string('JEC_Spring10_AK7Calo'), 
+         label  = cms.string('JEC_Spring10_AK7Calo') 
+      ),
+      cms.PSet(
+         record = cms.string('JEC_Spring10_AK7PF'), 
+         tag    = cms.string('JEC_Spring10_AK7PF'), 
+         label  = cms.string('JEC_Spring10_AK7PF') 
+      ),
+      cms.PSet(
+         record = cms.string('JEC_Spring10_IC5Calo'), 
+         tag    = cms.string('JEC_Spring10_IC5Calo'), 
+         label  = cms.string('JEC_Spring10_IC5Calo') 
       ),
      cms.PSet(
-         record = cms.string('IC5PF'), 
-         tag    = cms.string('IC5PF'), 
-         label  = cms.string('IC5PF') 
+         record = cms.string('JEC_Spring10_IC5PF'), 
+         tag    = cms.string('JEC_Spring10_IC5PF'), 
+         label  = cms.string('JEC_Spring10_IC5PF') 
       ),
       cms.PSet(
-         record = cms.string('KT4Calo'), 
-         tag    = cms.string('KT4Calo'), 
-         label  = cms.string('KT4Calo') 
+         record = cms.string('JEC_Spring10_KT4Calo'), 
+         tag    = cms.string('JEC_Spring10_KT4Calo'), 
+         label  = cms.string('JEC_Spring10_KT4Calo') 
       ),
       cms.PSet(
-         record = cms.string('KT4PF'), 
-         tag    = cms.string('KT4PF'), 
-         label  = cms.string('KT4PF') 
+         record = cms.string('JEC_Spring10_KT4PF'), 
+         tag    = cms.string('JEC_Spring10_KT4PF'), 
+         label  = cms.string('JEC_Spring10_KT4PF') 
       ),
       cms.PSet(
-         record = cms.string('KT6Calo'), 
-         tag    = cms.string('KT6Calo'), 
-         label  = cms.string('KT6Calo') 
+         record = cms.string('JEC_Spring10_KT6Calo'), 
+         tag    = cms.string('JEC_Spring10_KT6Calo'), 
+         label  = cms.string('JEC_Spring10_KT6Calo') 
       ),
       cms.PSet(
-         record = cms.string('KT6PF'), 
-         tag    = cms.string('KT6PF'), 
-         label  = cms.string('KT6PF') 
+         record = cms.string('JEC_Spring10_KT6PF'), 
+         tag    = cms.string('JEC_Spring10_KT6PF'), 
+         label  = cms.string('JEC_Spring10_KT6PF') 
       )
    ) 
 ) 
 
 process.dbWriterAK5Calo = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK5Calo') 
+   era    = cms.untracked.string('Spring10'), 
+   algo   = cms.untracked.string('AK5Calo') 
+) 
+process.dbWriterAK5CaloDataV2 = cms.EDAnalyzer('JetCorrectorDBWriter', 
+   era    = cms.untracked.string('Spring10DataV2'),  
+   algo   = cms.untracked.string('AK5Calo') 
 ) 
 process.dbWriterAK5PF = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK5PF') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('AK5PF') 
 ) 
+process.dbWriterAK5PFDataV2 = cms.EDAnalyzer('JetCorrectorDBWriter', 
+   era    = cms.untracked.string('Spring10DataV2'), 
+   algo   = cms.untracked.string('AK5PF') 
+)
 process.dbWriterAK5JPT = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK5JPT') 
+   era    = cms.untracked.string('Spring10'),  
+   algo  = cms.untracked.string('AK5JPT') 
+)
+process.dbWriterAK5JPTSUmmer10 = cms.EDAnalyzer('JetCorrectorDBWriter', 
+   era    = cms.untracked.string('Summer10'),  
+   algo  = cms.untracked.string('AK5JPT') 
+)
+process.dbWriterAK5JPTDataV2 = cms.EDAnalyzer('JetCorrectorDBWriter', 
+   era    = cms.untracked.string('Spring10DataV2'),  
+   algo   = cms.untracked.string('AK5JPT') 
 )
 process.dbWriterAK5TRK = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK5TRK') 
+   era    = cms.untracked.string('Spring10'), 
+   algo   = cms.untracked.string('AK5TRK') 
 )
 process.dbWriterAK7Calo = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK7Calo') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('AK7Calo') 
 ) 
 process.dbWriterAK7PF = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('AK7PF') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('AK7PF') 
 ) 
 process.dbWriterKT4Calo = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('KT4Calo') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('KT4Calo') 
 ) 
 process.dbWriterKT4PF = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('KT4PF') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('KT4PF') 
 ) 
 process.dbWriterKT6Calo = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('KT6Calo') 
+   era    = cms.untracked.string('Spring10'), 
+   algo   = cms.untracked.string('KT6Calo') 
 ) 
 process.dbWriterKT6PF = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('KT6PF') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('KT6PF') 
 )
 process.dbWriterIC5Calo = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('IC5Calo') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('IC5Calo') 
 ) 
 process.dbWriterIC5PF = cms.EDAnalyzer('JetCorrectorDBWriter', 
-   inputTxtFile = cms.untracked.string('Spring10'), 
-   option       = cms.untracked.string(''), 
-   label        = cms.untracked.string('IC5PF') 
+   era    = cms.untracked.string('Spring10'),  
+   algo   = cms.untracked.string('IC5PF') 
 ) 
 
 
 
 process.p = cms.Path( 
 process.dbWriterAK5Calo *
+process.dbWriterAK5CaloDataV2 *
 process.dbWriterAK5PF *
+process.dbWriterAK5PFDataV2 *
 process.dbWriterAK5JPT *
+process.dbWriterAK5JPTSUmmer10 *
+process.dbWriterAK5JPTDataV2 *
 process.dbWriterAK5TRK *
 process.dbWriterAK7Calo *
 process.dbWriterAK7PF *
