bf76fd5203fe9d2e83ec005bd209ec16be9ee8b4
cmsbuild <>
2011-12-07 19:04:44 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- Modules-V05-00-17-03'.
    Sprout from master 2011-12-07 19:04:43 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Adding V10 python and README files'
    Cherrypick from master 2011-06-07 14:29:12 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'add templates for the HLT configuration':
        interface/JetCorrectionService.h
        interface/JetCorrectionServiceChain.h
        python/hltJetCorrectionServiceChain_cfi.py
        python/hltL1FastjetCorrectionService_cfi.py
        python/hltLXXXCorrectionService_cfi.py
        src/JetCorrectionServiceChain.cc
    Cherrypick from master 2011-01-19 14:36:27 UTC Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch> 'Added the L1Offset correction service':
        interface/JetCorrectionProducer.h
        src/SealModule.cc
    Delete:
        interface/JetCorrectionESChain.h
        interface/JetCorrectionESProducer.h
        interface/JetCorrectionESSource.h
        python/hltJetCorrectionESChain_cfi.py
        python/hltL1FastjetCorrectionESProducer_cfi.py
        python/hltLXXXCorrectionESProducer_cfi.py
        src/JetCorrectionESChain.cc
        test/TauJetCorrectionDBReader_cfg.py
        test/TauJetCorrectionDBWriter_cfg.py

--- python/hltJetCorrectionServiceChain_cfi.py
+++ python/hltJetCorrectionServiceChain_cfi.py
@@ -1,10 +1,11 @@
-# HLT template for JetCorrectionESChain
+# HLT template for JetCorrectionServiceChain
 
 import FWCore.ParameterSet.Config as cms
 
-# L2L3 correction
-hltJetCorrectionESChain = cms.ESProducer( 'JetCorrectionESChain',
+# L2L3 correction service
+hltJetCorrectionServiceChain = cms.ESSource( 'JetCorrectionServiceChain',
     appendToDataLabel = cms.string( '' ),
+    label      = cms.string( 'hltJetCorrectionServiceChain' ),
     correctors = cms.vstring(
       'hltESSL2RelativeCorrectionService',
       'hltESSL3AbsoluteCorrectionService'
